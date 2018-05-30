2e9aa5b127ce0697145e55c369cd1bc3fdd59a21
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-30 20:22:37 +0530

    added a getter method that takes user input in playground.rb 1. takes in firsName 2. Uses string interpolation to print out sentence

--- playground.rb
+++ playground.rb
@@ -2,3 +2,8 @@ first_name = "Anish"
 last_name = "Sachdeva"
 
 puts first_name + " " + last_name
+
+puts "Enter your first Name : "
+firstName = gets.chomp
+
+puts "welcome " + firstName + " to the playground"
