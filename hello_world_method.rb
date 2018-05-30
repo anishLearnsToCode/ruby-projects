f6abfe07a34421e937f0bbbd2cec15322abace92
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-30 20:01:22 +0530

    created method in hello_world_method.rb file 1. created method helloWorld and 2. say_hello

---
+++ hello_world_method.rb
@@ -0,0 +1,18 @@
+# This is a method that will return hello world
+
+def helloWorld
+  "hello world"
+end
+
+def say_hello
+  puts "hello world"
+end
+
+puts helloWorld
+say_hello
+
+# Methods with arguments
+
+def say_hi(string)
+  puts string
+end
