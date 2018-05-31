4ff583c7d3d0d51fb2a80a2297c2525e81ff5001
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-31 09:14:48 +0530

    created file calculator.rb in ruby_projects 1. added method add to add 2 numbers 2. added method multiply to multiply 2 numbers 3. added method divide to divide 2 numbers 4. added method subtract to subtract to numbers

---
+++ calculator.rb
@@ -0,0 +1,16 @@
+def add(number1, number2)
+  number1.to_f + number2.to_f
+end
+
+def multiply(number1, number2)
+  number1.to_f * number2.to_f
+end
+
+def divide(divisor, dividend)
+  dividend.to_f / divisor.to_f
+end
+
+def subtract(number1, number2)
+  number1 - number2
+end
+
--- playground.rb
+++ playground.rb
@@ -3,7 +3,7 @@ last_name = "Sachdeva"
 
 puts first_name + " " + last_name
 
-puts "Enter your first Name : "
+puts 'Enter your first Name : '
 firstName = gets.chomp
 
 puts "welcome " + firstName + " to the playground"
