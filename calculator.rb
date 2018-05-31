b63f2c289679d5c2b7b59171e928d549e1681056
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-31 10:56:39 +0530

    created array_methods.rb file in ruby_projects dir

--- calculator.rb
+++ calculator.rb
@@ -14,3 +14,27 @@ def subtract(number1, number2)
   number1 - number2
 end
 
+puts 'Enter the operator you wish to use ?'
+puts '1) Add'
+puts '2) Subtract'
+puts '3) Multiply'
+puts '4) Divide'
+choice = gets.chomp.to_i
+
+puts'Enter first Number : '
+number1 = gets.chomp.to_f
+
+puts 'Enter second number : '
+number2 = gets.chomp.to_f
+
+if choice == 1
+  puts add(number1, number2)
+elsif choice == 2
+  puts subtract(number1, number2)
+elsif choice == 3
+  puts multiply(number1, number2)
+elsif choice == 4
+  puts divide(number1, number2)
+else
+  puts 'wrong choice'
+end
