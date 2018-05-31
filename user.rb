539baf91cf304758ed6f3d69ae2ce67a47fcf4c3
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-31 13:28:06 +0530

    create d attr_accessor variables in User class

--- user.rb
+++ user.rb
@@ -1,16 +1,24 @@
 class User
-  attr_accessor :name
+  attr_accessor :name, :email
 
-  def initialize(name)
+  def initialize(name, email)
     self.name = name
+    self.email = email
   end
 
   def to_s
-    self.name
+    'Name: ' + name + ' , Email: ' + email
   end
 end
 
-anish = User.new('Anish')
-puts anish
+anish = User.new('Anish', 'test')
+gautam = User.new('Gautam', 'test')
+piyush = User.new('Piyush', 'test')
 
+puts gautam
+puts gautam.name
+gautam.name = 'test'
+puts gautam.name
+
+puts
 puts User.ancestors
