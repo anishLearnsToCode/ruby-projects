97f15f41fc5f52ff82f6ddc9c8de5bf5fb9d8b56
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-05-31 16:55:16 +0530

    added json module to the User class using require 'json'

--- User Permission JSON Scanner/user.rb
+++ User Permission JSON Scanner/user.rb
@@ -1,2 +1,18 @@
+require 'json'
+
 class User
+  attr_accessor :name, :email, :permissions
+
+  def initialize(name, email)
+    @name = name
+    @email = email
+    @permissions = User.get_permissions_from_file
+  end
+
+  def self.get_permissions_from_file
+  end
+
+  def to_s
+    self.name
+  end
 end
